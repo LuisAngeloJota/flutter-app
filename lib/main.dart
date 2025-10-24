1b3763cdb6225370823d5e3c8a2a9bee5e8b51aa
LuisAngeloJota <cvcypw@gmail.com>
2025-10-24 19:00:10 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,42 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int counter = 0;
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('Test App 1'),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {
+                setState(() {
+                  counter++;
+                });
+              },
+            ),
+          ],
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(
+            'Hello World! $counter',
+            style: TextStyle(fontSize: 24),
+          ),
         ),
       ),
     );
